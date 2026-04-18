625f1682aab5f677aec2967bda8f5e605955d0ac
keyurprivate27 <keyurprivate27@gmail.com>
2026-04-18 09:02:25 +0530

    Updated with table

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <h1>Hello Radhe Krishna...!</h1>
 <h1>Good Morning...!</h1>
 <p>we are now learning CICD...!</p>
+
+<table>
+	<tr>
+		<th>Name</th>
+		<th>City</th>
+	</tr>
+	<tr>
+		<td>Keyur</td>
+		<td>Pune</td>
+	</tr>
+	<tr>
+		<td>Denish</td>
+		<td>Surat</td>
+	</tr>
+</table>
